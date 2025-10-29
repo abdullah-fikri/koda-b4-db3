f9d1fbb41c57ebde09bf36b1c5bbaaa6c61bd892
abdullah-fikri <abdullahfikri981@gmail.com>
2025-10-29 21:27:21 +0700

    feat: get the most productive director

--- query3.sql
+++ query3.sql
@@ -8,3 +8,11 @@ FROM actors
 JOIN roles ON actor_id = actors.id
 GROUP BY id, first_name 
 HAVING COUNT(id) > 5
+
+
+SELECT id, first_name, COUNT(id) AS paling_produktif
+FROM directors
+JOIN movies_directors ON director_id = directors.id
+GROUP BY id
+ORDER BY paling_produktif DESC
+LIMIT 1
